3dceeb4b9fdb11a3b9f8d25e83596950b402cf64
GingerJake <gingerjakey@outlook.com>
2023-02-25 21:58:58 -0500

    Move some stuff from Main to Game

--- src/main/java/org/gingerjake/potatogame/Game.java
+++ src/main/java/org/gingerjake/potatogame/Game.java
@@ -5,14 +5,13 @@ import org.gingerjake.potatogame.Levels.Hub;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Game extends JPanel implements Runnable {
     private boolean isRunning = false;
     private static boolean gameStarted = false;
     public static int width = 1600; //1600 //TODO: If aspect ratio is not 16:9, make black bars
-    // automatically.
-    public static int height = 900; //900 //TODO: Make it possible to change resolution without the
-    // game being sad, might fix with a title screen?
+    public static int height = 900;
     private static final double frameCap = 60.0; //TODO: Might be able to set in game settings?
     public static final boolean debug = true;
     private final StateManager sm = new StateManager();
@@ -21,6 +20,64 @@ public class Game extends JPanel implements Runnable {
     public Game() {
         setPreferredSize(new Dimension(width, height));
         setFocusable(true);
+
+        KeyboardFocusManager keyboardInput = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+
+        keyboardInput.addKeyEventDispatcher(e -> {
+            if (e.getID() == KeyEvent.KEY_PRESSED) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    Game.player.setUp(true);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    Game.player.setDown(true);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    Game.player.setLeft(true);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    Game.player.setRight(true);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    Game.player.attack("up");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_A) {
+                    Game.player.attack("left");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    Game.player.attack("down");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_D) {
+                    Game.player.attack("right");
+                }
+                if (e.getKeyCode() == KeyEvent.VK_P) {
+                    Game.player.hurt();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+                    Game.player.cancelAttack();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+                    Game.exit();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    Game.beginGame();
+                }
+            }
+            if (e.getID() == KeyEvent.KEY_RELEASED) {
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    Game.player.setUp(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    Game.player.setDown(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    Game.player.setLeft(false);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    Game.player.setRight(false);
+                }
+            }
+            return false;
+        });
 
         start();
     }
@@ -31,6 +88,7 @@ public class Game extends JPanel implements Runnable {
         height = getHeight();
         Thread thread = new Thread(this);
         thread.start();
+        System.out.println("Guess the game is running now.");
     }
 
     private void tick() {
--- src/main/java/org/gingerjake/potatogame/Main.java
+++ src/main/java/org/gingerjake/potatogame/Main.java
@@ -2,7 +2,6 @@ package org.gingerjake.potatogame;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,62 +14,5 @@ public class Main {
         gameFrame.add(new Game(), BorderLayout.CENTER);
         gameFrame.setResizable(true);
         gameFrame.setVisible(true);
-
-        KeyboardFocusManager keyManager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
-        keyManager.addKeyEventDispatcher(e -> {
-            if (e.getID() == KeyEvent.KEY_PRESSED) {
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    Game.player.setUp(true);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    Game.player.setDown(true);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    Game.player.setLeft(true);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    Game.player.setRight(true);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_W) {
-                    Game.player.attack("up");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_A) {
-                    Game.player.attack("left");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_S) {
-                    Game.player.attack("down");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_D) {
-                    Game.player.attack("right");
-                }
-                if (e.getKeyCode() == KeyEvent.VK_P) {
-                    Game.player.hurt();
-                }
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    Game.player.cancelAttack();
-                }
-                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-                    Game.exit();
-                }
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    Game.beginGame();
-                }
-            }
-            if (e.getID() == KeyEvent.KEY_RELEASED) {
-                if (e.getKeyCode() == KeyEvent.VK_UP) {
-                    Game.player.setUp(false);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    Game.player.setDown(false);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-                    Game.player.setLeft(false);
-                }
-                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-                    Game.player.setRight(false);
-                }
-            }
-            return false;
-        });
     }
 }
